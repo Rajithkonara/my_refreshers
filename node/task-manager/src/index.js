4b1c4a0f47d45f167219d7be8854eb94a5a25906
rajithkonara <rajith.konara@tokoin.io>
2020-06-06 22:09:19 +0530

    file uploads

--- node/task-manager/src/index.js
+++ node/task-manager/src/index.js
@@ -40,3 +40,12 @@ const myFunction = async () => {
 
 
 }
+
+const multer = require('multer')
+const upload = multer({
+    dest: 'images'
+})
+
+app.post('/upload', upload.single('upload'), (req,res) => {
+    res.send()
+})
--- node/task-manager/src/routers/user.js
+++ node/task-manager/src/routers/user.js
@@ -2,6 +2,9 @@ const express = require('express')
 const User = require('../models/user')
 const router = new express.Router()
 const auth = require('../middleware/auth')
+const multer = require('multer')
+const sharp = require('sharp')
+
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
@@ -111,6 +114,62 @@ router.delete('/users/me', auth, async (req, res) => {
         res.send(req.user)
     } catch (e) {
         return res.status(500).send(e)
+    }
+})
+
+const upload = multer({
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('Please upload a image'))
+        }
+
+        cb(undefined, true)
+
+        // cb(new Error('File must be a image'))
+        // cb(undefined, true)
+        // cb(undefined, false)
+    }
+})
+
+
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    try {
+        const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
+        req.user.avatar = buffer
+        await req.user.save()
+        res.sendStatus(200)
+    } catch (error) {
+        return res.status(500).send(error)
+    }
+
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
+})
+
+
+router.delete('/users/me/avatar', auth, async (req, res) => {
+    req.user.avatar = undefined
+    await req.user.save()
+    res.sendStatus(200)
+})
+
+router.get('/users/:id/avatar', async (req, res, ) => {
+    try {
+        const user = await User.findById(req.params.id)
+
+        if (!user || !user.avatar) {
+            throw new Error()
+        }
+
+        res.set('Content-Type', 'image/png')
+        res.send(user.avatar)
+
+    } catch (error) {
+        return res.status(404).send(error)
     }
 })
 
