bce5cea9650b3529a5493dac00682147058c4783
rajithkonara <rajith.konara@tokoin.io>
2020-06-22 08:54:22 +0530

    event ack

--- node/chat-app/public/js/chat.js
+++ node/chat-app/public/js/chat.js
@@ -9,7 +9,33 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
 
     const message = e.target.elements.message.value
 
-    socket.emit('sendMessage', message)
+    socket.emit('sendMessage', message), (error) => {
+        //ack
+
+        if (error) {
+            return console.log(error)
+        }
+
+        console.log('This message was Delivered!')
+    }
+})
+
+
+document.querySelector('#send-location').addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser')
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation', { 
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+         }, () =>{
+             console.log('Location shared')
+         })
+    })
+
+
 })
 
 // socket.on('countUpdated', (count) => {
--- node/chat-app/src/index.js
+++ node/chat-app/src/index.js
@@ -3,6 +3,7 @@ const path = require('path')
 const http = require('http')
 const app = express()
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 const server = http.createServer(app)
 
@@ -26,13 +27,29 @@ io.on('connection', (socket) => {
 
     socket.broadcast.emit('message', 'A new user has joined')
 
-    socket.on('sendMessage', (message) => {
+    socket.on('sendMessage', (message, callback) => {
+        
+        const filter = new Filter()
+        
+        if (filter.isProfane(message)) {
+            return callback('Profanity is not allowed')
+        }
+    
         io.emit('message', message)
+        //ack
+        callback()
+    })
+
+
+    socket.on('sendLocation', (coords, callback) => {
+        io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+        callback()
     })
 
     socket.on('disconnect', () => {
         io.emit('message', 'A user has left')
     })
+
 })
 
 
